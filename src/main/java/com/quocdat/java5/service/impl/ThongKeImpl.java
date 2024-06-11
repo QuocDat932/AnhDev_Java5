38ad6199d7d47921472bf5d619b9425f16b45554
hoangha <hoangha01627381018@gmail.com>
2024-06-11 11:59:24 +0700

    HoangHa_commit08: update ThongKe

--- src/main/java/com/quocdat/java5/service/impl/ThongKeImpl.java
+++ src/main/java/com/quocdat/java5/service/impl/ThongKeImpl.java
@@ -7,7 +7,9 @@ import com.quocdat.java5.service.ThongKeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -19,13 +21,40 @@ public class ThongKeImpl  implements ThongKeService {
         if (listLichSuHocTap.isEmpty()) {
             return null;
         }
-        long soLuongPass = listLichSuHocTap
-                .stream()
-                .filter(lichSuHocTap -> "Passed".equals(lichSuHocTap.getKetQua()))
-                .count();
-        long tiLePass = soLuongPass*100/listLichSuHocTap.size();
+        Map<Object , Integer> result = new HashMap<>();
+        result.put(0.0,0);
+        result.put(1.0,0);
+        result.put(2.0,0);
+        result.put(3.0,0);
+        result.put(4.0,0);
+        result.put(5.0,0);
+        result.put(6.0,0);
+        result.put(7.0,0);
+        result.put(8.0,0);
+        result.put(9.0,0);
+        result.put(10.0,0);
+        listLichSuHocTap.forEach(lichSuHocTap -> {
+            result.put(lichSuHocTap.getKetQua() , result.get(lichSuHocTap.getKetQua()) == null ? 1 : result.get(lichSuHocTap.getKetQua()) + 1 );
+            result.put((double)Math.round(lichSuHocTap.getDiemThi()),
+                    result.get(
+                            (double)Math.round(lichSuHocTap.getDiemThi())) + 1 );
+        });
+        long tiLePass = result.get("Passed") * 100 / listLichSuHocTap.size();
         long tiLeFail = 100-tiLePass;
-        return new ThongKeM().setTiLeFail(tiLeFail).setTiLePass(tiLePass);
+        return new ThongKeM()
+                .setTiLeFail(tiLeFail)
+                .setTiLePass(tiLePass)
+                .setDiem0(result.get(0.0))
+                .setDiem1(result.get(1.0))
+                .setDiem2(result.get(2.0))
+                .setDiem3(result.get(3.0))
+                .setDiem4(result.get(4.0))
+                .setDiem5(result.get(5.0))
+                .setDiem6(result.get(6.0))
+                .setDiem7(result.get(7.0))
+                .setDiem8(result.get(8.0))
+                .setDiem9(result.get(9.0))
+                .setDiem10(result.get(10.0));
     }
 }
 
